b1744eb1aeeb2e089bab5e73bd803301965a40ca
AYUSH KUMAR <ayushkr1511@gmail.com>
2024-06-14 11:43:51 +0530

    Implemented browse video background

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -1,10 +1,14 @@
 import Header from "./Header";
 import useCurPlayingMovies from "../hooks/useCurPlayingMovies";
+import MainContainer from "./MainContainer";
+import SecondaryContainer from "./SecondaryContainer";
 const Browse = () => {
   useCurPlayingMovies();
   return (
     <div>
       <Header />
+      <MainContainer />
+      <SecondaryContainer />
     </div>
   );
 };
---
+++ src/components/VideoTitle.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const VideoTitle = ({ title, overview }) => {
+  return (
+    <div className="w-screen aspect-video absolute pt-[20%] px-24 text-white bg-gradient-to-r  from-black">
+      <h1 className="text-4xl font-bold ">{title}</h1>
+      <p className="py-4 w-1/3 text-lg">{overview}</p>
+      <div>
+        <button className="bg-white text-black px-10 py-3 text-lg  rounded-lg hover:bg-opacity-80">
+          ▶ Play
+        </button>
+        <button className="bg-gray-500 mx-2 text-white px-5 py-3 text-lg bg-opacity-70 rounded-lg">
+          ℹ More Info
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default VideoTitle;
